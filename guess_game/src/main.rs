f82ce0d2bd037b059773796ce14a743b1d2dc161
tonarinohito <rsp44316@gmail.com>
2019-10-03 23:23:59 +0900

    数当てゲーム

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,8 +1,15 @@
+extern crate rand;  //外部のクレートを使うよ宣言
+
+//includeとかusing的な奴
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("数当てゲーム!");
-
+    //乱数生成
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("秘密の数字は: {}です", secret_number);
     println!("数字を入力");
 
     //rustはデフォルトでconstなので、変更したい場合はmutを付ける
@@ -16,5 +23,14 @@ fn main() {
         .expect("read_lineに失敗");
 
     // {} はプレースホルダで変数の中身を出力する
-    println!("入力した値は: {}です", guess);
+    println!("入力した値は: {}", guess);
+    //rustの変数は途中で型を変えることができる
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+    //パターンマッチ
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("ちいさい!"),
+        Ordering::Greater => println!("おおきい!"),
+        Ordering::Equal   => println!("あたり!"),
+    }
 }
