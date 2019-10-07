ae67cf9f614878ff304be56b5df7b68166fe9a18
tonarinohito <rsp44316@gmail.com>
2019-10-07 22:01:50 +0900

    プロジェクト追加
    ついでに入力待ちも対応

---
+++ dining_philosophers/src/main.rs
@@ -0,0 +1,63 @@
+/** https://doc.rust-jp.rs/the-rust-programming-language-ja/1.6/book/dining-philosophers.html */
+use std::thread;
+use std::time::Duration;
+use device_query::{DeviceQuery, DeviceState, Keycode};  //キー入力を使用可能にする
+
+struct Philosopher 
+{
+    name: String,
+}
+
+impl Philosopher 
+{
+    //構造体の関連関数(メソッドではないC++でいう名前空間に近い)->は戻り値
+    fn new(name: &str) -> Philosopher 
+    {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+    //自身の参照を引数に取るとメソッドになる
+    fn eat(&self) 
+    {
+        
+        println!("{} は食べています.", self.name);
+        thread::sleep(Duration::from_millis(100));
+        println!("{} は食べ終わりました.", self.name);
+    }
+}
+
+fn main() 
+{
+
+    let philosophers = vec!
+    [
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+
+    for p in &philosophers 
+    {
+        p.eat();
+    }
+    println!("何かキーを押すと終了します");
+    loop
+    {
+        let device_state = DeviceState::new();
+        let keys: Vec<Keycode> = device_state.get_keys();
+        
+        if !keys.is_empty()
+        {
+            break;
+        }
+        // for key in keys.iter()
+        // {
+        //     //{:?} では fmt::Debug の実装が使われる()
+        //     //構造体の中身をダンプできる
+        //     println!("{:?}を押しました", key);
+        // }
+    }
+}
